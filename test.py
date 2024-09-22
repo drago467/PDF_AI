3844d40bc77511c67b4cb0dbe7bbafcb71cabc1e
drago467 <tdt.trandinhtuan467@gmail.com>
2024-09-22 16:40:43 +0700

    Receving Chunks with a Callback

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
+from langchain.callbacks.base import BaseCallbackHandler
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI(streaming = True)
+class StreamingHandler(BaseCallbackHandler):
+    def on_llm_new_token(self, token, **kwargs):
+        pass
+
+chat = ChatOpenAI(
+    streaming = True,
+    callbacks = [StreamingHandler()]
+)
 
 prompt = ChatPromptTemplate.from_messages([
     ("human", "{content}")
