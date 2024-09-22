d43b907f75b5e2a29fe763f142cc47fc515230a9
drago467 <tdt.trandinhtuan467@gmail.com>
2024-09-22 15:10:23 +0700

    testing the chain

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,5 +1,16 @@
 from app.chat.models import ChatArgs
 from app.chat.vector_scores.pinecone import build_retriever
+from langchain.chains import ConversationalRetrievalChain
+from app.chat.llms.chatopenai import build_llm
+from app.chat.memories.sql_memory import build_memory
 
 def build_chat(chat_args: ChatArgs):
     retriever = build_retriever(chat_args)
+    llm = build_llm(chat_args)
+    memory = build_memory(chat_args)
+
+    return ConversationalRetrievalChain.from_llm(
+        llm = llm,
+        memory = memory,
+        retriever = retriever
+    )
