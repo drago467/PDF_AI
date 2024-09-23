fa6ebdd3c823a2d19521c0f4d7bfaa5094d42da8
drago467 <tdt.trandinhtuan467@gmail.com>
2024-09-23 23:44:14 +0700

    Calculating the average score

--- app/chat/score.py
+++ app/chat/score.py
@@ -40,4 +40,25 @@ def get_scores():
     pass
 
 def random_component_by_score(component_type, component_map):
-    pass
+    # Make sure componenet_type is 'llm', 'retriever', or 'memory'
+    if component_type not in ["llm", "retriever", "memory"]:
+        raise ValueError("Invalid component_type")
+    
+    # From redis, get the hash containing the sum total scores for the given component_type
+    values = client.hgetall(f"{component_type}_score_values")
+
+    # From reid, get the has containing  the number of times each component has been voted on 
+    counts = client.hgetall(f"{component_type}_score_counts")
+
+    # Get all the valid component names from the component map
+    names = component_map.keys()
+
+    # Loop over those valid names and use them to calculate the average score for each
+    # Add the average score to a dictionary
+    avg_scores = {}
+    for name in names:
+        score = int(values.get(name, 1))
+        count = int(counts.get(name, 1))
+        avg = score / count
+        avg_scores[name] = max(avg, 0.1)
+    # Do a weighted random selection
